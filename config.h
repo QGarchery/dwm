8ccd6c9d928b74566e3c0bf2d4e41d8241d8ebbb
Quentin Garchery <garchery.quentin@gmail.com>
2021-11-03 17:17:17 +0100

    shortcut to restart dwm (restartsig patch)

--- config.h
+++ config.h
@@ -69,6 +69,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
